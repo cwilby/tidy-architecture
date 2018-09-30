a566eab3b052b8b16347fba1be23e3d8aa668bf9
Cameron Wilby <cwilby@sendlane.com>
2018-09-30 16:05:23 -0700

    Start refactor to support laravel.

--- index.js
+++ index.js
@@ -10,103 +10,105 @@ var pluralize = require('pluralize');
 worker();
 
 function worker() {
-    buildDomainClasses();
-    buildRepositoryInterfaces();
-    buildRepositoryImplementations();
-    buildControllers();
+    buildModels();
+    buildRepositories();
 }
 
-function buildDomainClasses() {
-    config.entities.forEach(function(entity) {
-        buildFile({
-            template: './templates/Domain.mustache',
-            entity: entity,
-            filePath: '{{solution}}\\{{rootNamespace}}.Core\\Domain\\{{entityFolder}}',
-            fileName: '{{entityName}}.cs'
-        });
-    });
+function buildModels() {
+    config.models.forEach(buildModel);
 }
 
-function buildRepositoryInterfaces() {
-    config.entities.forEach(function(entity) {
-        buildFile({
-            template: './templates/IEntityRepository.mustache',
-            entity: entity,
-            filePath: '{{solution}}\\{{rootNamespace}}.Core\\Repository\\{{entityFolder}}',
-            fileName: 'I{{entityName}}Repository.cs'
-        });
-    });
+function buildRepositories() {
+    config.models.forEach(buildRepository);
 }
 
-function buildRepositoryImplementations() {
-    config.entities.forEach(function(entity) {
-        buildFile({
-            template: './templates/EntityRepository.mustache',
-            entity: entity,
-            filePath: '{{solution}}\\{{rootNamespace}}.Data\\Repository\\{{entityFolder}}',
-            fileName: '{{entityName}}Repository.cs'
-        });
-    });
-}
+function buildModel(model) {
+    fs.readFile('./templates/Model.mustache', { encoding: 'utf-8' }, (err, template) => {
+        if(err) return console.error(err);
 
-function buildControllers() {
-    config.entities.forEach(function(entity) {
-        buildFile({
-            template: './templates/Controller.mustache',
-            entity: entity,
-            filePath: '{{solution}}\\{{rootNamespace}}.Api\\Controllers\\{{entityFolder}}',
-            fileName: '{{entityNamePluralized}}Controller.cs'
-        });
-    });
-}
+        const output = mustache.render(template, model);
+        
+        const filePath = `${root}/Models/${model.area}/${model.module}`;
+        const fileName = `${filePath}/${model.name}.php`;
 
-function buildFile({ template='', entity={}, filePath='', fileName='' } = {}) {
-    let entitySplit = _.split(entity, '.');
-    let entityName = _.last(entitySplit);
-    let namespaceSplit = _.take(entitySplit, entitySplit.length - 1);
-
-    let view = {
-        solution: config.solution,
-        rootNamespace: config.rootNamespace,
-        entityFolder: _.join(namespaceSplit, '\\'),
-        entityNamespace: _.join(namespaceSplit, '.'),
-        entityName: entityName,
-        entityNameLower: _.camelCase(entityName),
-        entityNamePluralized: pluralize(entityName, 2),
-        entityNameLowerPluralized: pluralize(_.camelCase(entityName), 2),
-        entityClass: entityName
-    };
-
-    if (entitySplit.length > 1 && view.entityName === entitySplit.slice(-2)[0]) {
-        view.entityClass = `Core.Domain.${view.entityNamespace}.${view.entityName}`;
-    }
-
-    fs.readFile(template, { encoding: 'utf-8' }, function(err, template) {
-        if (err) {
-            return console.log(err);
-        }
-
-        let output = mustache.render(template, view);
-
-        filePath = mustache.render(filePath, view);
-        fileName = mustache.render(fileName, view);
-
-        if (!fs.existsSync(`${filePath}\\${fileName}`)) {
-            mkdirp(filePath, function(err) {
-                if (err) {
-                    return console.log(err);
+        if(!fs.existsSync(fileName)) {
+            mkdirp(filePath, (err) => {
+                if(err) {
+                    return console.error(err);
                 }
 
-                fs.writeFile(`${filePath}\\${fileName}`, output, function(err) {
-                    if (err) {
+                fs.writeFile(fileName, output, (err) => {
+                    if(err) {
                         return console.log(err);
                     }
 
-                    console.log('Wrote ' + fileName);
+                    console.log(`Add ${fileName}`);
                 });
-            });
+            });    
         } else {
-            console.log('Skipping ' + filePath)
+            console.log(`Skip ${fileName}`);
         }
     });
+}
+
+function buildRepository(model) {
+    fs.readFile('./templates/Repository.mustache', { encoding: 'utf-8' }, (err, template) => {
+        if(err) return console.error(err);
+
+        const output = mustache.render(template, model);
+
+        const filePath = `${root}/Repositories/${model.area}/${model.module}`;
+        const fileName = `${filePath}/${model.name}Repository.php`;
+        
+        if(!fs.existsSync(fileName)) {
+            mkdirp(filePath, (err) => {
+                if(err) {
+                    return console.error(err);
+                }
+
+
+                fs.writeFile(fileName, output, (err) => {
+                    if(err) {
+                        return console.log(err);
+                    }
+
+                    console.log(`Add ${fileName}`);
+                });
+            });    
+        } else {
+            console.log(`Skip ${fileName}`);
+        }
+    });
+}
+
+function createBuilder(type, template) {
+    return function buildFile(model) {
+        fs.readFile(template, { encoding: 'utf-8' }, (err, template) => {
+            if(err) return console.error(err);
+    
+            const output = mustache.render(template, model);
+    
+            const filePath = `${root}/Repositories/${model.area}/${model.module}`;
+            const fileName = `${filePath}/${model.name}Repository.php`;
+            
+            if(!fs.existsSync(fileName)) {
+                mkdirp(filePath, (err) => {
+                    if(err) {
+                        return console.error(err);
+                    }
+    
+    
+                    fs.writeFile(fileName, output, (err) => {
+                        if(err) {
+                            return console.log(err);
+                        }
+    
+                        console.log(`Add ${fileName}`);
+                    });
+                });    
+            } else {
+                console.log(`Skip ${fileName}`);
+            }
+        });
+    }
 }
